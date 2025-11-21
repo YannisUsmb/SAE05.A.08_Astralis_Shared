290ebac8e299c31de5f5b02c740f4e573e418988
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-21 16:35:05 +0100

    Added ChangePasswordDto.

---
+++ Astralis.Shared/DTOs/Users/ChangePasswordDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs.Users
+{
+    public class ChangePasswordDto
+    {
+        [Required(ErrorMessage = "The current password is required.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "The new password is required.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "The new password must be at least 8 characters long.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).+$", ErrorMessage = "The password must contain at least one uppercase letter, one number, and one special character.")]
+        public string NewPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please confirm the new password.")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword), ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; } = null!;
+    }
+}
