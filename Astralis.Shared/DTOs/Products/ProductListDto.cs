609c3e9d2b8f109f780cfed2663d92f85bd6b10f
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-21 10:57:43 +0100

    Updated ProductListDto.

--- Astralis.Shared/DTOs/Products/ProductListDto.cs
+++ Astralis.Shared/DTOs/Products/ProductListDto.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Label { get; set; } = null!;
         public decimal Price { get; set; }
-        public string CategoryName { get; set; } = null!;
+
+        // Category Info.
+        public int ProductCategoryId { get; set; }
+        public string CategoryLabel { get; set; } = null!;
     }
 }
