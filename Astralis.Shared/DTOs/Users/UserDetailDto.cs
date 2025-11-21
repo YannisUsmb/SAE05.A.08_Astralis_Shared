dca7a962301ff76ca1183633efa78faadd3a2191
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-21 11:44:13 +0100

    Unfinished UserDetailDto.

---
+++ Astralis.Shared/DTOs/Users/UserDetailDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class UserDetailDto
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Phone { get; set; }
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public DateOnly InscriptionDate { get; set; }
+        public char? Gender { get; set; }
+
+        public int UserRoleId { get; set; }
+    }
+}
