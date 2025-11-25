2a8fb458df120593bf88e4e3591375298acf93f0
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 09:18:05 +0100

    feat(dtos): add ReportUpdateDto for moderation status handling.

---
+++ Astralis.Shared/DTOs/Reports/ReportUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class ReportUpdateDto
+    {
+        [Required(ErrorMessage = "The report ID is required.")]
+        public int Id { get; set; }
+
+        // No need to have admin ID here, it will be taken from the authenticated user context.
+
+        [Required(ErrorMessage = "The report status ID is required.")]
+        public int ReportStatusId { get; set; }
+    }
+}
