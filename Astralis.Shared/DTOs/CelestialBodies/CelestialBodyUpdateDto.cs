859c47e89fa208727cd9edcee732c0d9e2e9f8fe
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 17:13:11 +0100

    feat(dtos): add CelestialBodyUpdateDto for generic renaming.

---
+++ Astralis.Shared/DTOs/CelestialBodies/CelestialBodyUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class CelestialBodyUpdateDto
+    {
+        [Required(ErrorMessage = "The name is required.")]
+        [StringLength(100, ErrorMessage = "The name cannot be longer than 100 characters.")]
+        public string Name { get; set; } = null!;
+
+        [StringLength(100, ErrorMessage = "The alias cannot be longer than 100 characters.")]
+        public string? Alias { get; set; }
+    }
+}
