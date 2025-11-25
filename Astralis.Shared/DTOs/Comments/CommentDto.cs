6edc64851c51f93566343f98b49e8cef7d421ec3
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 11:28:17 +0100

    feat(dtos): add CommentDto with flattened structure for frontend tree building.

---
+++ Astralis.Shared/DTOs/Comments/CommentDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Astralis.Shared.DTOs.Comments
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public int ArticleId { get; set; }
+
+        // To reconstruct the thread on the Frontend.
+        public int? RepliesToId { get; set; }
+
+        public string Text { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public bool IsVisible { get; set; }
+
+        // User info.
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string UserAvatarUrl { get; set; } = null!;
+    }
+}
