6e7c229a210794febebcb7fafce945bc2a2e8a65
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 08:29:44 +0100

    feat(dtos): add NotificationCreateDto for system creation.

---
+++ Astralis.Shared/DTOs/Notifications/NotificationCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs.Notifications
+{
+    public class NotificationCreateDto
+    {
+        [Required(ErrorMessage = "The label is required.")]
+        [StringLength(100, ErrorMessage = "The label cannot be longer than 100 characters.")]
+        public string Label { get; set; } = null!;
+
+        [StringLength(300, ErrorMessage = "The description cannot be longer than 300 characters.")]
+        public string? Description { get; set; }
+    }
+}
