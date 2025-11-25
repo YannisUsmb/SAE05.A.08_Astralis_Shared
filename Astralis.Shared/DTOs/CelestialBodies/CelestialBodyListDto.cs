e7871839bc637303830a420a021c3b5229dde4a2
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 17:16:23 +0100

    feat(dtos): add CelestialBodyListDto for generic search and listing.

---
+++ Astralis.Shared/DTOs/CelestialBodies/CelestialBodyListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CelestialBodyListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Alias { get; set; }
+
+        // Celestial Body Type Info.
+        public int CelestialBodyTypeId { get; set; }
+        public string CelestialBodyTypeName { get; set; } = null!;
+    }
+}
