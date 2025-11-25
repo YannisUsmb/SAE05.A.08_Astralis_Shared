b4be27219af4a1255ce8b6a326bd5b455440e56f
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 17:08:33 +0100

    feat(dtos): add CelestialBodyCreateDto for base entity creation.

---
+++ Astralis.Shared/DTOs/CelestialBodies/CelestialBodyCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class CelestialBodyCreateDto
+    {
+        [Required(ErrorMessage = "The celestial body type ID is required.")]
+        public int CelestialBodyTypeId { get; set; }
+
+        [Required(ErrorMessage = "The name is required.")]
+        [StringLength(100, ErrorMessage = "The name cannot be longer than 100 characters.")]
+        public string Name { get; set; } = null!;
+
+        [StringLength(100, ErrorMessage = "The alias cannot be longer than 100 characters.")]
+        public string? Alias { get; set; }
+    }
+}
