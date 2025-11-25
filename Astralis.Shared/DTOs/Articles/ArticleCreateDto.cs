a0e43148670a153b1ddf99c05a0d5c86862ea0af
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 16:26:28 +0100

    feat(dtos): add ArticleCreateDto with category assignment support.

---
+++ Astralis.Shared/DTOs/Articles/ArticleCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class ArticleCreateDto
+    {
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(50, ErrorMessage = "The title cannot be longer than 50 characters.")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "The content is required.")]
+        public string Content { get; set; } = null!;
+
+        [Required(ErrorMessage = "The premium status is required.")]
+        public bool IsPremium { get; set; }
+
+        public List<int> CategoryIds { get; set; } = new List<int>();
+    }
+}
