5898f6345482adc519513fd727251edaccc49358
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 16:36:20 +0100

    feat(dtos): add ArticleDetailDto with full content and category listing.

---
+++ Astralis.Shared/DTOs/Articles/ArticleDetailDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Astralis.Shared.DTOs.Articles
+{
+    public class ArticleDetailDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public bool IsPremium { get; set; }
+
+        // Author Info.
+        public int UserId { get; set; }
+        public string AuthorName { get; set; } = null!;
+        public string AuthorAvatarPath { get; set; } = null!;
+
+        // Categories Info.
+        public List<int> CategoryIds { get; set; } = new List<int>();
+        public List<string> CategoryNames { get; set; } = new List<string>();       
+    }
+}
