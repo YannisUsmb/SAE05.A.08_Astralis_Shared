cc4a00a0c40986a2eacfed3e424d183f378e5bbe
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 11:05:22 +0100

    feat(dtos): add EventDto with flattened organizer info and interest stats.

---
+++ Astralis.Shared/DTOs/Events/EventDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class EventDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? Location { get; set; }
+        public string? Link { get; set; }
+
+        // Event Type Flattening.
+        public int EventTypeId { get; set; }
+        public string EventTypeLabel { get; set; } = null!; // Ex: "Concert", "Workshop"
+
+        public int UserId { get; set; }
+
+        // Calculated Property EventInterests.Count()
+        public int InterestCount { get; set; }
+    }
+}
