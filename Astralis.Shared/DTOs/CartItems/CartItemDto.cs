61a3f9b856ce34be41394e13771bf78780421edc
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 09:38:19 +0100

    feat(dtos): add CartItemDto with flattened product details for cart view.

---
+++ Astralis.Shared/DTOs/CartItems/CartItemDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CartItemDto
+    {
+        // Cart Item details.
+        public int Quantity { get; set; }
+
+        // Product details.
+        public int ProductId { get; set; }
+        public string ProductLabel { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+
+        // Calculated Property, not mapped to database.
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+}
