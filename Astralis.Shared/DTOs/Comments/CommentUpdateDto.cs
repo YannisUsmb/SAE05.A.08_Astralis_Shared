242aeff8c2fa68dce8a78d0684e833057a9f3b14
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 11:20:03 +0100

    feat(dtos): add CommentUpdateDto for content editing.

---
+++ Astralis.Shared/DTOs/Comments/CommentUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class CommentUpdateDto
+    {
+        [Required(ErrorMessage = "The text is required.")]
+        [StringLength(300, ErrorMessage = "The text cannot be longer than 300 characters.")]
+        public string Text { get; set; } = null!;
+    }
+}
