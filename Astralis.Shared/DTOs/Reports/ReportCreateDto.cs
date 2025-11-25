21ae318ec318a9ed590781fb792a4836f61f86dc
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 09:14:52 +0100

    feat(dtos): add ReportCreateDto for user reporting logic.

---
+++ Astralis.Shared/DTOs/Reports/ReportCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class ReportCreateDto
+    {
+        [Required(ErrorMessage = "The comment ID is required.")]
+        public int CommentId { get; set; }
+
+        [Required(ErrorMessage = "The report motive ID is required.")]
+        public int ReportMotiveId { get; set; }
+
+        [Required(ErrorMessage = "The description is required.")]
+        [StringLength(150, ErrorMessage = "The description cannot be longer than 150 characters.")]
+        public string Description { get; set; } = null!;
+    }
+}
