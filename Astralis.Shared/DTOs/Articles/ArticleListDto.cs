bc5aa6eadcd289a9e3fd202544c2bee57d6697ae
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 16:31:29 +0100

    feat(dtos): add ArticleListDto with content preview and author details.

---
+++ Astralis.Shared/DTOs/Articles/ArticleListDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class ArticleListDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public bool IsPremium { get; set; }
+
+        // Content substring from the Controller.
+        public string Preview { get; set; } = null!;
+
+        // Author Info.
+        public int UserId { get; set; }
+        public string AuthorName { get; set; } = null!;
+        public string AuthorAvatarPath { get; set; } = null!;
+
+        // Computed properties.
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+    }
+}
