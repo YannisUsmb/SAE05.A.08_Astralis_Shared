f412263d5be0bd9cdd540aaf4397d251a25b1709
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 16:18:03 +0100

    feat(dtos): add AddressDto with flattened city details and country details.
    Implemented AddressDto including CityName, PostCode and CountryName directly to avoid additional API calls for city and country informations.

---
+++ Astralis.Shared/DTOs/Addresses/AddressDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class AddressDto
+    {
+        public int Id { get; set; }
+        public string StreetNumber { get; set; } = null!;
+        public string StreetAddress { get; set; } = null!;
+
+        // City Info.
+        public int CityId { get; set; }
+        public string CityName { get; set; } = null!;
+        public string PostCode { get; set; } = null!;
+
+        // Country Info.
+        public int CountryId { get; set; }
+        public string CountryName { get; set; } = null!;
+    }
+}
