56794664a1cf4849eb5a6f2322b2796eb4068182
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 09:51:46 +0100

    feat(dtos): add CartDto wrapper for total price calculation.

---
+++ Astralis.Shared/DTOs/CartItems/CartDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CartDto
+    {
+        public List<CartItemDto> Items { get; set; } = new();
+
+        // Calculated Properties, not mapped to database.
+        public decimal GrandTotal => Items.Sum(i => i.TotalPrice);
+        public int TotalItemsCount => Items.Sum(i => i.Quantity);
+    }
+}
