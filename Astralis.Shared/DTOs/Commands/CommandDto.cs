4a8cc999a1f0462fd920bc45516a7b448011c9b2
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 10:18:36 +0100

    feat(dtos): add CommandDto with flattened status and order details.

---
+++ Astralis.Shared/DTOs/Commands/CommandDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CommandDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Total { get; set; }
+
+        // URL of the PDF invoice.
+        public string PdfName { get; set; } = null!;
+
+        // Command status details.
+        public int CommandStatusId { get; set; }
+        public string CommandStatusLabel { get; set; } = null!; // e.g., "Pending", "Shipped", etc.
+
+        // Collection of order details associated with this command.
+        public ICollection<OrderDetailDto> OrderDetails { get; set; } = new List<OrderDetailDto>();
+    }
+}
