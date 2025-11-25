a7d0aab92edd52ae671cb0bc8bc573d93ba99820
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 16:11:25 +0100

    feat(dtos): add AddressUpdateDto for location modification.

---
+++ Astralis.Shared/DTOs/Addresses/AddressUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class AddressUpdateDto
+    {
+        [Required(ErrorMessage = "The city ID is required.")]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "The street number is required.")]
+        [StringLength(15, ErrorMessage = "The street number cannot be longer than 15 characters.")]
+        public string StreetNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "The street address is required.")]
+        [StringLength(200, ErrorMessage = "The street address cannot be longer than 200 characters.")]
+        public string StreetAddress { get; set; } = null!;
+    }
+}
