e3f7024d8b910b5f4552ac62203e5d178dc9b3f4
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-25 10:46:16 +0100

    feat(dtos): add OrderDetailDto to display command items.

---
+++ Astralis.Shared/DTOs/OrderDetails/OrderDetailDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class OrderDetailDto
+    {
+        // Properties representing order detail information.
+        public int ProductId { get; set; }
+        public string ProductLabel { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+
+        // Quantity of the product ordered.
+        public int Quantity { get; set; }
+
+        // Computed property for total line price.
+        public decimal TotalLinePrice => Quantity * UnitPrice;
+    }
+}
