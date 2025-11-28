74240ed248ccc9047627113d04a1ff9864aaa1f3
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 13:16:36 +0100

    feat(dtos): add DiscoveryCreateDto for user submissions.

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryCreateDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryCreateDto
+    {
+        [Required(ErrorMessage = "The celestial body ID is required.")]
+        public int CelestialBodyId { get; set; }
+
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(100, ErrorMessage = "The title cannot be longer than 100 characters.")]
+        public string Title { get; set; } = null!;
+    }
+}
