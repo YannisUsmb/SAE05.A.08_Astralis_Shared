d49362d721a1ac0c1d20958ffb2e35df61da4bec
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 13:31:33 +0100

    feat(dtos): add DiscoveryDto with flattened user and status details.
    Implemented DiscoveryDto aggregating information about the submitter, the celestial body, and the current workflow status (Discovery/Alias) for display.

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+
+        // Disovery Details.
+        public int CelestialBodyId { get; set; }
+        public string CelestialBodyName { get; set; } = null!;
+
+        // Author Details.
+        public int UserId { get; set; }
+        public string UserName { get; set; } = null!;
+
+        // Status Details.
+        public int DiscoveryStatusId { get; set; }
+        public string DiscoveryStatusLabel { get; set; } = null!;
+
+        public int? AliasStatusId { get; set; }
+        public string? AliasStatusLabel { get; set; }
+    }
+}
