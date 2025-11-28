6c37fb356ff935745ae1026a7b2a805fd01dff7e
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 11:41:50 +0100

    feat(dtos): add AsteroidDto for reading asteroid details.
    Implemented AsteroidDto with flattened properties from CelestialBody (Name, Alias) and OrbitalClass to facilitate data display without requiring extra API calls.

---
+++ Astralis.Shared/DTOs/Asteroid/AsteroidDto.cs
@@ -0,0 +1,36 @@
+﻿namespace Astralis.Shared.DTOs.Asteroid
+{
+    public class AsteroidDto
+    {
+        public int Id { get; set; }
+
+        // Celestial Body Properties.
+        public int CelestialBodyId { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Alias { get; set; }
+
+        // Orbital Class Properties.
+        public int OrbitalClassId { get; set; }
+        public string OrbitalClassName { get; set; } = null!;
+        public string OrbitalClassDescription { get; set; } = null!;
+
+        // Scientific Properties.
+        public string? Reference { get; set; }
+        public bool? IsPotentiallyHazardous { get; set; }
+
+        public decimal? AbsoluteMagnitude { get; set; }
+        public decimal? DiameterMinKm { get; set; }
+        public decimal? DiameterMaxKm { get; set; }
+
+        public int? OrbitId { get; set; }
+
+        // Dates.
+        public DateTime? OrbitDeterminationDate { get; set; }
+        public DateTime? FirstObservationDate { get; set; }
+        public DateTime? LastObservationDate { get; set; }
+
+        // Orbital Properties.
+        public decimal? SemiMajorAxis { get; set; }
+        public decimal? Inclination { get; set; }
+    }
+}
