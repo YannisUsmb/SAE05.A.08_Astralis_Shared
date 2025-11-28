f48ff5424b80a77abb272e83e88d42e3853da960
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 13:26:57 +0100

    feat(dtos): add DiscoveryModerationDto for admin approval workflow.

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryModerationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryModerationDto
+    {
+        [Required(ErrorMessage = "The discovery status ID is required.")]
+        public int DiscoveryStatusId { get; set; }
+
+        public int AliasStatusId { get; set; }
+    }
+}
