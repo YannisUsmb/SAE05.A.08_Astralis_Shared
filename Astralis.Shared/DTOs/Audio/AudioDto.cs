347c37feddd2768d7449a39a09a691fc12c1d9a2
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 11:10:02 +0100

    feat(dto): add AudioDto.

---
+++ Astralis.Shared/DTOs/Audio/AudioDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class AudioDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string FilePath { get; set; } = null!;
+        public string CelestialBodyTypeLabel { get; set; } = null!;
+    }
+}
