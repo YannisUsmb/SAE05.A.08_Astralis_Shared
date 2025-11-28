a41e985c43e1d1285259688da47353c00b6f55a2
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 13:55:56 +0100

    refactor(namespaces): update namespaces to align with project structure.

--- Astralis.Shared/DTOs/GalaxyQuasar/GalaxyQuasarCreateDto.cs
+++ Astralis.Shared/DTOs/GalaxyQuasar/GalaxyQuasarCreateDto.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Astralis.Shared.DTOs.GalaxyQuasar
+namespace Astralis.Shared.DTOs
 {
     public class GalaxyQuasarCreateDto : CelestialBodyCreateDto
     {
