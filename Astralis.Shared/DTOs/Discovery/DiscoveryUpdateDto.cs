9186c82363b8f4aceb1c8c05041c459bac4a0b47
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 13:20:19 +0100

    feat(dtos): add DiscoveryUpdateDto for editing submission details.

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryUpdateDto
+    {
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(100, ErrorMessage = "The title cannot be longer than 100 characters.")]
+        public string Title { get; set; } = null!;
+    }
+}
