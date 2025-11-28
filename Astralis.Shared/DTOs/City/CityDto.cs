b34bd68635c884c5f379c074c77d82266ad57106
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 10:57:22 +0100

    feat(dto): add CityDto with geographic coordinates.

---
+++ Astralis.Shared/DTOs/City/CityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string PostCode { get; set; } = null!;
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+}
