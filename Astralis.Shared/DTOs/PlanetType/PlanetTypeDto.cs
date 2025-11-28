a32666d39bbafca6651cc3133b088b4b31acd705
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-11-28 14:07:08 +0100

    feat(dtos): add PlanetTypeDto for category selection dropdowns.

---
+++ Astralis.Shared/DTOs/PlanetType/PlanetTypeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class PlanetTypeDto
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
