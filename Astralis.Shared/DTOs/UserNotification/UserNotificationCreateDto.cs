e36a01bf3dadb57e221a32e9e5f96a5ac89511fd
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-02 13:53:17 +0100

    feat(dtos): add UserNotificationCreate.

---
+++ Astralis.Shared/DTOs/UserNotification/UserNotificationCreateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class UserNotificationCreateDto
+    {
+        public int UserId { get; set; }
+        public int NotificationId { get; set; }
+
+        [Required(ErrorMessage = "The read status is required.")]
+        public bool IsRead { get; set; }
+
+        [Required(ErrorMessage = "The receiving date is required.")]
+        public DateTime ReceivedAt { get; set; }
+    }
+}
