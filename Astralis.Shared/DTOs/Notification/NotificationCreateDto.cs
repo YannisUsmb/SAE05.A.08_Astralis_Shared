ee244581a051505f61827d9ad8f7f892d9ae64fa
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-02 14:17:01 +0100

    refactor(dtos): add notification type property in NotificationCreateDto.

--- Astralis.Shared/DTOs/Notification/NotificationCreateDto.cs
+++ Astralis.Shared/DTOs/Notification/NotificationCreateDto.cs
@@ -4,6 +4,9 @@ namespace Astralis.Shared.DTOs
 {
     public class NotificationCreateDto
     {
+        [Required(ErrorMessage = "The notification type ID is required.")]
+        public int NotificationTypeId { get; set; }
+
         [Required(ErrorMessage = "The label is required.")]
         [StringLength(100, ErrorMessage = "The label cannot be longer than 100 characters.")]
         public string Label { get; set; } = null!;
