d70053c9cc9c5551e6ee9a73cfec0bd660f158e5
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-02 14:34:24 +0100

    feat(dtos): add MailboxDto with a list of notifications linked to the user.

---
+++ Astralis.Shared/DTOs/UserNotification/MailboxDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class MailboxDto
+    {
+        public List<UserNotificationDto> Notifications { get; set; } = new();
+
+        // Calculated Properties, not mapped to database.
+        public int TotalItemsCount => Notifications.Count;
+    }
+}
