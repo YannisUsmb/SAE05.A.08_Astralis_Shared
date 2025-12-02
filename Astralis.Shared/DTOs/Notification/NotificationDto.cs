eb6588debf20a84c9d9ffbccab1dce64b7e6ce0f
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-02 14:23:54 +0100

    feat(dtos): add NotificationDto with notification type flattening.

---
+++ Astralis.Shared/DTOs/Notification/NotificationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class NotificationDto
+    {
+        public int Id { get; set; }
+
+        // Notification Type Properties.
+        public int NotificationTypeId { get; set; }
+        public string NotificationTypeName { get; set; } = null!;
+
+        public string Label { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
