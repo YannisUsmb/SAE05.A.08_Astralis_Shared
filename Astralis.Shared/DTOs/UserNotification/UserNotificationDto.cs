1ff38cfb9b3e766bb267173619bb059c5b803c71
barriefl <Florian.Barrier@etu.univ-smb.fr>
2025-12-02 13:31:52 +0100

    feat(dtos): add UserNotificationDto for notification.

---
+++ Astralis.Shared/DTOs/UserNotification/UserNotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class UserNotificationDto
+    {
+        public int UserId { get; set; }
+        public int NotificationId { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime ReceivedAt { get; set; }
+    }
+}
