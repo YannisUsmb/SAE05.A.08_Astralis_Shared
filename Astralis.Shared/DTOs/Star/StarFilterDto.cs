d74c3831661d380760b0063fafd9a428222dc97a
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:56:49 +0100

    feat(dtos): add StarFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Star/StarFilterDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class StarFilterDto
+    {
+        public string? Name { get; set; }
+        public List<int>? SpectralClassIds { get; set; }
+
+        public string? Constellation { get; set; }
+        public string? Designation { get; set; }
+        public string? BayerDesignation { get; set; }
+
+        public decimal? MinDistance { get; set; }
+        public decimal? MaxDistance { get; set; }
+
+        public decimal? MinLuminosity { get; set; }
+        public decimal? MaxLuminosity { get; set; }
+
+        public decimal? MinRadius { get; set; }
+        public decimal? MaxRadius { get; set; }
+
+        public decimal? MinTemperature { get; set; }
+        public decimal? MaxTemperature { get; set; }
+    }
+}
