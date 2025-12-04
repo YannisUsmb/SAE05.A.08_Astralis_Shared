9859bd08f17572f2a5a0b442940ef6f2694a903d
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:55:30 +0100

    feat(dtos): add DiscoveryFilterDto and GalaxyQuasarFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryFilterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryFilterDto
+    {
+        public string? Title { get; set; }
+        public int? DiscoveryStatusId { get; set; }
+        public int? AliasStatusId { get; set; }
+        public int? DiscoveryApprovalUserId { get; set; }
+        public int? AliasApprovalUserId { get; set; }
+    }
+}
---
+++ Astralis.Shared/DTOs/GalaxyQuasar/GalaxyQuasarFilterDto.cs
@@ -0,0 +1,23 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class GalaxyQuasarFilterDto
+    {
+        public string? Reference { get; set; }
+        public List<int>? GalaxyQuasarClassIds { get; set; }
+
+        public decimal? MinRightAscension { get; set; }
+        public decimal? MaxRightAscension { get; set; }
+
+        public decimal? MinDeclination { get; set; }
+        public decimal? MaxDeclination { get; set; }
+
+        public decimal? MinRedshift { get; set; }
+        public decimal? MaxRedshift { get; set; }
+
+        public decimal? MinRMagnitude { get; set; }
+        public decimal? MaxRMagnitude { get; set; }
+
+        public int? MinMjdObs { get; set; }
+        public int? MaxMjdObs { get; set; }
+    }
+}
