5e6990b8ce54a2fb0d1a8b2015eb856be33d1533
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:53:45 +0100

    feat(dtos): add CelestialBodyFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/CelestialBody/CelestialBodyFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CelestialBodyFilterDto
+    {
+        public string? SearchText { get; set; }
+        public List<int>? CelestialBodyTypeIds { get; set; }
+        public bool? IsDiscovery { get; set; }
+    }
+}
