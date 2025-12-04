b4b09da743dd6adfe63c7800beab8b90c90a6204
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:56:26 +0100

    feat(dtos): add SatelliteFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Satellite/SatelliteFilterDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class SatelliteFilterDto
+    {
+        public string? Name { get; set; }
+        public List<int>? PlanetIds { get; set; }
+
+        public decimal? MinGravity { get; set; }
+        public decimal? MaxGravity { get; set; }
+
+        public decimal? MinRadius { get; set; }
+        public decimal? MaxRadius { get; set; }
+
+        public decimal? MinDensity { get; set; }
+        public decimal? MaxDensity { get; set; }
+    }
+}
