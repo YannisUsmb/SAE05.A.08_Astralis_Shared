afc6b49a4291ee15b93f723234004c15484ee9e4
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:57:19 +0100

    feat(dtos): add ReportFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Report/ReportFilterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class ReportFilterDto
+    {
+        public int? StatusId { get; set; }
+        public int? MotiveId { get; set; }
+        public DateTime? MinDate { get; set; }
+        public DateTime? MaxDate { get; set; }
+    }
+}
