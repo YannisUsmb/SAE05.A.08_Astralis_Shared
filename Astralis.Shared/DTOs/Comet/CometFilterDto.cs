5c2310b0d29aafb6733b2fdcb527d20a3ca51c1d
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:54:16 +0100

    feat(dtos): add CometFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Comet/CometFilterDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class CometFilterDto
+    {
+        public string? Reference { get; set; }
+
+        public decimal? MinEccentricity { get; set; }
+        public decimal? MaxEccentricity { get; set; }
+
+        public decimal? MinInclination { get; set; }
+        public decimal? MaxInclination { get; set; }
+
+        public decimal? MinPerihelionAU { get; set; }
+        public decimal? MaxPerihelionAU { get; set; }
+
+        public decimal? MinAphelionAU { get; set; }
+        public decimal? MaxAphelionAU { get; set; }
+
+        public decimal? MinOrbitalPeriod { get; set; }
+        public decimal? MaxOrbitalPeriod { get; set; }
+
+        public decimal? MinMOID { get; set; }
+        public decimal? MaxMOID { get; set; }
+    }
+}
