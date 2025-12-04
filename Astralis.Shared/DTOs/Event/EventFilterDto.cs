e06303121e4c1e93eb71df76eb52cfda4a853360
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:41:55 +0100

    feat(dtos): add EventFilterDto for advanced search capabilities.

---
+++ Astralis.Shared/DTOs/Event/EventFilterDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class EventFilterDto
+    {
+        public string? SearchText { get; set; }
+        public List<int>? EventTypeIds { get; set; }
+        public DateTime? MinStartDate { get; set; }
+        public DateTime? MaxStartDate { get; set; }
+        public DateTime? MinEndDate { get; set; }
+        public DateTime? MaxEndDate { get; set; }
+    }
+}
