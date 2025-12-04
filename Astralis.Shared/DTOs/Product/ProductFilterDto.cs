aaf7b04f61b3af15f48ee91bd334b12b7f459f25
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:57:48 +0100

    feat(dtos): add ProductFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Product/ProductFilterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class ProductFilterDto
+    {
+        public string? SearchText { get; set; }
+        public List<int>? ProductCategoryIds { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+    }
+}
