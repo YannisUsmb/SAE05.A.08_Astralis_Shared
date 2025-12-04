f26086cc20bb572ec2c86fc5cf6f178e0e52eebb
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:52:41 +0100

    feat(dtos): add AsteroidFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Asteroid/AsteroidFilterDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class AsteroidFilterDto
+    {
+        public string? Reference { get; set; }
+        public List<int>? OrbitalClassIds { get; set; }
+        public bool? IsPotentiallyHazardous { get; set; }
+        public int? OrbitId { get; set; }
+
+        public decimal? MinAbsoluteMagnitude { get; set; }
+        public decimal? MaxAbsoluteMagnitude { get; set; }
+
+        public decimal? MinDiameter { get; set; }
+        public decimal? MaxDiameter { get; set; }
+
+        public decimal? MinInclination { get; set; }
+        public decimal? MaxInclination { get; set; }
+
+        public decimal? MinSemiMajorAxis { get; set; }
+        public decimal? MaxSemiMajorAxis { get; set; }
+    }
+}
