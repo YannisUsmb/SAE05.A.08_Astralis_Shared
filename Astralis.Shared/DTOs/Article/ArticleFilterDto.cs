8f6c2595f4ddbd3a7e02eefdcdcf5ace43ea0653
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-04 10:52:10 +0100

    feat(dtos): add ArticleFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Article/ArticleFilterDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class ArticleFilterDto
+    {
+        public string? SearchTerm { get; set; }
+        public List<int>? TypeIds { get; set; }
+        public bool? IsPremium { get; set; }
+    }
+}
