47c2a58c2ebee8a473ef9925f658672cb494f62f
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-06 17:52:41 +0100

    feat(dto) : added missing OrbitalClassDto

---
+++ Astralis.Shared/DTOs/OrbitalClass/OrbitalClassDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class OrbitalClassDto
+    {
+        public int Id { get; set; }
+        public string Label { get; set; } = null!;
+        public string Description { get; set; } = null!;
+    }
+}
