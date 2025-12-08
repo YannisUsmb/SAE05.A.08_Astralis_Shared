908d95eca4bad01ef5b6f5b0d42e9c5e8f8dd511
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-08 16:47:40 +0100

    fix(dto) : fix PlanetFilterDto after changes with Planet model

--- Astralis.Shared/DTOs/Planet/PlanetFilterDto.cs
+++ Astralis.Shared/DTOs/Planet/PlanetFilterDto.cs
@@ -3,29 +3,19 @@
     public class PlanetFilterDto
     {
         public string? Name { get; set; }
-
-        // List of Ids for filtering.
         public List<int>? PlanetTypeIds { get; set; }
         public List<int>? DetectionMethodIds { get; set; }
-
-        // Filtres numériques (Ranges)
+        public decimal? MinDistance { get; set; }
+        public decimal? MaxDistance { get; set; }
+        public decimal? MinMass { get; set; }
+        public decimal? MaxMass { get; set; }
+        public decimal? MinRadius { get; set; }
+        public decimal? MaxRadius { get; set; }
         public int? MinDiscoveryYear { get; set; }
         public int? MaxDiscoveryYear { get; set; }
-
-        public decimal? MinOrbitalPeriod { get; set; }
-        public decimal? MaxOrbitalPeriod { get; set; }
-
         public decimal? MinEccentricity { get; set; }
         public decimal? MaxEccentricity { get; set; }
-
         public decimal? MinStellarMagnitude { get; set; }
         public decimal? MaxStellarMagnitude { get; set; }
-
-        // Partial match filters.
-        public string? Distance { get; set; }
-        public string? Radius { get; set; }
-        public string? Temperature { get; set; }
-        public string? HostStarTemperature { get; set; }
-        public string? HostStarMass { get; set; }
     }
 }
