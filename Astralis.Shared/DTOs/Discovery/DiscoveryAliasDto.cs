c0e3b1cf290b3b421f22d12dfb94c22307877dd5
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-08 14:08:57 +0100

    feat(dto) : added DiscoveryAliasDto to handle alias management

---
+++ Astralis.Shared/DTOs/Discovery/DiscoveryAliasDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class DiscoveryAliasDto
+    {
+        [Required(ErrorMessage = "The alias is required.")]
+        [StringLength(100, ErrorMessage = "The alias cannot be longer than 100 characters.")]
+        public string Alias { get; set; } = null!;
+    }
+}
