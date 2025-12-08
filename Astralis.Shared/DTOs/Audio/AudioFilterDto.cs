e347e78433047d90354bedc1a5fd0002ce3a6ee8
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-08 14:44:49 +0100

    feat(dtos): add AudioFilterDto for advanced search capabilities

---
+++ Astralis.Shared/DTOs/Audio/AudioFilterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Astralis.Shared.DTOs
+{
+    public class AudioFilterDto
+    {
+        public string? SearchTerm { get; set; }
+        public List<int>? CelestialBodyTypeIds { get; set; }
+    }
+}
