4d6c699233dcc168ed5d8e8a31ec12b807cffab3
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2025-12-17 16:50:45 +0100

    feat(dto) : add CountryName to PhonePrefixDto

--- Astralis.Shared/DTOs/PhonePrefix/PhonePrefixDto.cs
+++ Astralis.Shared/DTOs/PhonePrefix/PhonePrefixDto.cs
@@ -5,6 +5,7 @@ namespace Astralis.Shared.DTOs
     {
         public int Id { get; set; }
         public string Label { get; set; } = null!;
+        public string? CountryName { get; set; }
 
         public override bool Equals(object? obj)
         {
