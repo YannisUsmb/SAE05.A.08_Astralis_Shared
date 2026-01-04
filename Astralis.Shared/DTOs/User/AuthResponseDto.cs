f7e67054751bdab1ed1d2b7cbc41c4e4d840af8a
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2026-01-04 03:25:05 +0100

    feat(dto) : add Id in AuthResponseDto

--- Astralis.Shared/DTOs/User/AuthResponseDto.cs
+++ Astralis.Shared/DTOs/User/AuthResponseDto.cs
@@ -2,6 +2,7 @@
 {
     public class AuthResponseDto
     {
+        public int Id { get; set; }
         public string Token { get; set; } = null!;
         public DateTime Expiration { get; set; }
 
