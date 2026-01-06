745bfbcc0a0ae91033f3c9a0d5243b266bf5ffca
EnzoBenechIUT <enzo.benech@etu.univ-smb.fr>
2026-01-06 16:46:43 +0100

    fix(add): added properties to product filter dto

--- Astralis.Shared/DTOs/Product/ProductFilterDto.cs
+++ Astralis.Shared/DTOs/Product/ProductFilterDto.cs
@@ -7,6 +7,8 @@ namespace Astralis.Shared.DTOs
         public List<int>? ProductCategoryIds { get; set; }
         public decimal? MinPrice { get; set; }
         public decimal? MaxPrice { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 30;
 
         public override bool Equals(object? obj)
         {
