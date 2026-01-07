497b50c9d3a651a7c9c931000eff0b824bd78234
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2026-01-07 03:53:56 +0100

    feat(dto) : add ForgotPasswordDto and ResetPasswordDto

---
+++ Astralis.Shared/DTOs/User/ForgotPasswordDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class ForgotPasswordDto
+    {
+        [Required(ErrorMessage = "L'email est requis.")]
+        [EmailAddress(ErrorMessage = "Format d'email invalide.")]
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ Astralis.Shared/DTOs/User/ResetPasswordDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Astralis.Shared.DTOs
+{
+    public class ResetPasswordDto
+    {
+        [Required]
+        public string Token { get; set; } = null!;
+
+        [Required(ErrorMessage = "Le nouveau mot de passe est requis.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Le mot de passe doit faire entre 8 et 100 caractères.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).+$", ErrorMessage = "Le mot de passe doit contenir une majuscule, un chiffre et un caractère spécial.")]
+        public string NewPassword { get; set; } = null!;
+
+        [Compare(nameof(NewPassword), ErrorMessage = "Les mots de passe ne correspondent pas.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
