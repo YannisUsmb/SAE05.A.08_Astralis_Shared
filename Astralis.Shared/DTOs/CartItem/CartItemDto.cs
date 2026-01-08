69cbe048cf535b1a3b0f310845c838d1dcfdba8e
enzo <155959665+EnzoBenechIUT@users.noreply.github.com>
2026-01-08 22:12:11 +0100

    Add ProductPictureUrl to CartItemDto
    Added ProductPictureUrl property to CartItemDto.

--- Astralis.Shared/DTOs/CartItem/CartItemDto.cs
+++ Astralis.Shared/DTOs/CartItem/CartItemDto.cs
@@ -1,17 +1,12 @@
-﻿
 namespace Astralis.Shared.DTOs
 {
     public class CartItemDto
     {
-        // Cart Item details.
         public int Quantity { get; set; }
-
-        // Product details.
         public int ProductId { get; set; }
         public string ProductLabel { get; set; } = null!;
         public decimal UnitPrice { get; set; }
-
-        // Calculated Property, not mapped to database.
+        public string ProductPictureUrl { get; set; } = null!;
         public decimal TotalPrice => Quantity * UnitPrice;
 
         public override bool Equals(object? obj)
