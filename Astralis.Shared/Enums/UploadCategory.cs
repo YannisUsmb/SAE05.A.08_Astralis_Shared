6ec51d8b18a999d20abbc40317ff562abe7a2b4d
YannisUsmb <yannis.mauline@etu.univ-smb.fr>
2026-01-08 21:21:50 +0100

    feat(enum) : add UploadCategory enum to handle image uploads logic

---
+++ Astralis.Shared/Enums/UploadCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace Astralis.Shared.Enums
+{
+    public enum UploadCategory
+    {
+        Avatars,
+        Products,
+        Articles,
+        Events,
+        Asteroids,
+        Comets,
+        Galaxies,
+        Planets,
+        Quasars,
+        Satellites,
+        Stars
+    }
+}
